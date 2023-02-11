c74dfa328a55cd1f8f1b64c674df4d9332255bec
RamoIsReaLoL <85399076+RamoIsReaLoL@users.noreply.github.com>
2023-02-11 18:47:00 +0300

    Update lanyard.js

--- assets/js/lanyard.js
+++ assets/js/lanyard.js
@@ -55,10 +55,10 @@ function update_presence() {
         <a href="javascript:void(0)">
         <div class="card rounded-xl h-full">
             <div class="p-3 flex space-x-2 items-center overflow-hidden">
-                 <img src="/assets/img/disneyPlus.png" alt="IMG" class="rounded-xl"
+                 <img src="https://cdn.discordapp.com/avatars/881986210191523920/a_fb93e962113018299075ff4b5668f792.png?size=1024" alt="IMG" class="rounded-xl"
                      width="50" height="50">
-                 <p class="normalText ml-3 opacity-90">Disney+<br><span class="smallText opacity-80">1221</span></p>
-                 <p class="thinText sectionTopRightText rounded-xl p-2 opacity-80">1223</p>
+                 <p class="normalText ml-3 opacity-90">Disney+<br><span class="smallText opacity-80">${RamoIsReaLoLActivity.details}</span></p>
+                 <p class="thinText sectionTopRightText rounded-xl p-2 opacity-80">${new Date(api.d.timestamps.start).getTime();}</p>
             </div>
        </div>
        </a>`;
